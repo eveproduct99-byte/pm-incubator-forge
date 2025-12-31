05f7076c2e8f47508622a0fd43fb4b6e44512a4f
chaewon-byte <chaewon.kr@gmail.com>
2025-12-31 16:12:48 +0900

    feat: [이름] 배열 자르기(slice)[PMIF-x]

--- sonchaewon/javascript/javascript-app/src/App.tsx
+++ sonchaewon/javascript/javascript-app/src/App.tsx
@@ -11,6 +11,8 @@ import { ControlFlowSumExample } from './control_flow/ControlFlowSumExample'
 import { SecondProblem } from './problem/SecondProblem'
 import { MapExample } from './map/MapExample'
 import { MapReduceExample } from './map/MapReduceExample'
+import { MapFilterExample } from './map/MapFilterExample'
+import { ArraySliceExample } from './array/ArraySliceExample'
 
 function App() {
  
@@ -32,6 +34,8 @@ function App() {
         <SecondProblem/>
         <MapExample/>
         <MapReduceExample/>
+        <MapFilterExample/>
+        <ArraySliceExample/>
        </div>
     </>
   )
--- sonchaewon/javascript/javascript-app/src/map/MapReduceExample.tsx
+++ sonchaewon/javascript/javascript-app/src/map/MapReduceExample.tsx
@@ -3,6 +3,7 @@ export const MapReduceExample = () =>{
 
    //reduce 연산과정
    //<(누산 대상, 배열의요소) => 누산대상+배열요소, 초기값)
+   //accumulator의 시작값은 입력한 초기값으로 설정된
    //실행 효율 Te관점
    //앞서 했던 for Loop 구성을 단순화 시킨 작업
    let result_array: number = 
